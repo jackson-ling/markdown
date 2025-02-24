d2a195206674b1ff59d09597d2d1cde257103dd7
jackson-ling <1524205121@qq.com>
2025-02-24 22:22:45 +0800

    first commit

---
+++ Chapter 1/cmake-build-debug/CMakeFiles/B2025.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "B2025.exe"
+  "B2025.exe.manifest"
+  "B2025.pdb"
+  "CMakeFiles/B2025.dir/B2025.c.obj"
+  "libB2025.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/B2025.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter 1/cmake-build-debug/CMakeFiles/Chapter_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_1.dir/B2002.c.obj"
+  "Chapter_1.exe"
+  "Chapter_1.exe.manifest"
+  "Chapter_1.pdb"
+  "libChapter_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chapter_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
